b16f8ac5415c5cb483c2e6ac02a3973282ce8b35
elidelima <eli.lima@alpar.com.br>
2018-03-08 12:20:22 -0300

    teste 999

--- recipeapp-angularjs/src/main/java/guru/springframework/recipe/RecipeappApplication.java
+++ recipeapp-angularjs/src/main/java/guru/springframework/recipe/RecipeappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecipeappApplication {
 
 	public static void main(String[] args) {
+		//initialize
 		SpringApplication.run(RecipeappApplication.class, args);
 	}
 }
